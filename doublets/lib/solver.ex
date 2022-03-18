bf5d1640e9dcfa484801753c7fb3d04af8888082
erickbarcenas <barcenas.dev@gmail.com>
2022-03-18 05:46:55 -0600

    cleaning

--- doublets/lib/solver.ex
+++ doublets/lib/solver.ex
@@ -6,8 +6,33 @@ defmodule Doublets.Solver do
       |> Enum.to_list
   end
 
-  def doublets(word1, word2) do
-    "make me work"
+
+
+  def clean(file) do
+    file
+    # |> String.normalize(:nfd) # "á" -> "a", "´"
+    #|> String.replace(~r/[^A-z\s]/u, "")
+
+    |> Enum.map(fn x->
+      String.replace(x, "\n", "")
+      |> String.downcase()
+      |> String.split(~r/[^[:alnum:]-]/u, trim: true)
+     end)
+    |> List.flatten()
+  end
+
+  def doublets(starting_word, ending_word) do
+    starting_word = starting_word |> String.downcase()
+    ending_word = ending_word |> String.downcase()
+
+    len_sw = starting_word |> String.length()
+    len_ew = ending_word |> String.length()
+    if len_sw != len_ew, do: IO.inspect({:error, "Words aren't the same lengths"})
+
+    # Check if the words are in the dictionary
+    words
+    |> clean()
+
   end
 
 end
